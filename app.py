61a6343db48a677f8b99050060e13a52ba772f3a
LyzaIamrache <li2mn@umkc.edu>
2026-05-04 15:33:55 -0100

    Rename latency fields to include seconds

--- app.py
+++ app.py
@@ -195,17 +195,17 @@ if uploaded_file is not None:
         total_latency = round(time.time() - start_time, 2)
 
         # Evaluation
-        evaluation = {
-            "audio_file": uploaded_file.name,
-            "device": device,
-            "transcription_word_count": len(transcription.split()),
-            "summary_word_count": len(summary.split()),
-            "key_points_count": count_key_points(key_points),
-            "study_questions_count": count_questions(study_questions),
-            "asr_latency": asr_latency,
-            "generation_latency": generation_latency,
-            "total_latency": total_latency
-        }
+    evaluation = {
+    "audio_file": uploaded_file.name,
+    "device": device,
+    "transcription_word_count": len(transcription.split()),
+    "summary_word_count": len(summary.split()),
+    "key_points_count": count_key_points(key_points),
+    "study_questions_count": count_questions(study_questions),
+    "asr_latency_seconds": asr_latency,
+    "generation_latency_seconds": generation_latency,
+    "total_latency_seconds": total_latency
+}
 
         evaluation["quality_score"] = quality_score(evaluation)
 
